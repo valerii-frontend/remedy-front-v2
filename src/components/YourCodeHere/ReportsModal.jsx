806d5d13c7e70cc7556566e8b6ff68f5c573deeb
v.lozghachov <lvaldev.code@gmail.com>
2023-08-20 00:13:49 +0200

    split table

--- src/components/YourCodeHere/ReportsModal.jsx
+++ src/components/YourCodeHere/ReportsModal.jsx
@@ -26,7 +26,6 @@ const ModalActions = () => (
     <button className="UIButton UIButton--green">Start Reviewing</button>
   </div>
 );
-
 const TableRow = ({ title, children }) => (
   <tr>
     <td className="ReportsModal__table-title">{title}</td>
@@ -42,6 +41,55 @@ const BugSummary = () => (
     <h3 className="Bug-summary__title">Impact</h3>
     <div className="Bug-summary__code">CODE</div>
   </div>
+);
+const ModalTable = () => (
+  <table className="ReportsModal__table">
+    <tbody>
+      <TableRow title="Report ID">%RBB9934567%</TableRow>
+      <TableRow title="Submission Date">21 Feb 2023</TableRow>
+      <TableRow title="Program Name">Most epic contest 2023</TableRow>
+      <TableRow title="Asset">
+        <span>
+          Proxy and implementation
+          <IconSvg name="angle-arrow" />,
+        </span>{" "}
+        <span>
+          AaveDebtPositionParserType
+          <IconSvg name="angle-arrow" />,
+        </span>{" "}
+        <span>
+          AaveDebtPositionParserType
+          <IconSvg name="angle-arrow" />
+        </span>
+      </TableRow>
+      <TableRow title="Severity">Critical</TableRow>
+      <TableRow title="Report content">
+        <BugSummary />
+      </TableRow>
+      <TableRow title="Additional links">
+        <span>
+          https://www.figma.com/
+          <IconSvg name="angle-arrow" />,
+        </span>{" "}
+        <span>
+          https://immunefi.com/.com/
+          <IconSvg name="angle-arrow" />,
+        </span>{" "}
+        <span>
+          AaveDebtPositionParserType
+          <IconSvg name="angle-arrow" />
+        </span>
+      </TableRow>
+      <TableRow title="Additional links">
+        <div className="ReportsModal__screenshots">
+          <div className="ReportsModal__screenshot">F1 screenshot.png</div>
+          <div className="ReportsModal__screenshot">F2 screenshot.png</div>
+          <div className="ReportsModal__screenshot">F1 screenshot.png</div>
+          <div className="ReportsModal__screenshot">F2 screenshot.png</div>
+        </div>
+      </TableRow>
+    </tbody>
+  </table>
 );
 export default function ReportsModal({ close }) {
   return (
@@ -59,61 +107,7 @@ export default function ReportsModal({ close }) {
           <UserAvatar />
           <ModalActions />
         </div>
-        <table className="ReportsModal__table">
-          <tbody>
-            <TableRow title="Report ID">%RBB9934567%</TableRow>
-            <TableRow title="Submission Date">21 Feb 2023</TableRow>
-            <TableRow title="Program Name">Most epic contest 2023</TableRow>
-            <TableRow title="Asset">
-              <span>
-                Proxy and implementation
-                <IconSvg name="angle-arrow" />,
-              </span>{" "}
-              <span>
-                AaveDebtPositionParserType
-                <IconSvg name="angle-arrow" />,
-              </span>{" "}
-              <span>
-                AaveDebtPositionParserType
-                <IconSvg name="angle-arrow" />
-              </span>
-            </TableRow>
-            <TableRow title="Severity">Critical</TableRow>
-            <TableRow title="Report content">
-              <BugSummary />
-            </TableRow>
-            <TableRow title="Additional links">
-              <span>
-                https://www.figma.com/
-                <IconSvg name="angle-arrow" />,
-              </span>{" "}
-              <span>
-                https://immunefi.com/.com/
-                <IconSvg name="angle-arrow" />,
-              </span>{" "}
-              <span>
-                AaveDebtPositionParserType
-                <IconSvg name="angle-arrow" />
-              </span>
-            </TableRow>
-            <TableRow title="Additional links">
-              <div className="ReportsModal__screenshots">
-                <div className="ReportsModal__screenshot">
-                  F1 screenshot.png
-                </div>
-                <div className="ReportsModal__screenshot">
-                  F2 screenshot.png
-                </div>
-                <div className="ReportsModal__screenshot">
-                  F1 screenshot.png
-                </div>
-                <div className="ReportsModal__screenshot">
-                  F2 screenshot.png
-                </div>
-              </div>
-            </TableRow>
-          </tbody>
-        </table>
+        <ModalTable />
         <div className="ReportsModal__footer d-flex justify-content-end">
           <ModalActions />
         </div>
