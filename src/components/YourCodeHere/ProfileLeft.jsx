275cec5fe71dd118c3bd32a1bb4cf3c1f8c27c64
v.lozghachov <lvaldev.code@gmail.com>
2023-09-09 23:09:10 +0200

    add activity items

--- src/components/YourCodeHere/ProfileLeft.jsx
+++ src/components/YourCodeHere/ProfileLeft.jsx
@@ -1,4 +1,6 @@
 import photoImg from "../../assets/images/profile/avatar-photo.png";
+import avatar1 from "../../assets/images/profile/avatar.png";
+import avatar2 from "../../assets/images/profile/avatar2.png";
 import IconSvg from "./IconSvg";
 
 const ProfileHeaderSocialLink = ({ children }) => (
@@ -176,7 +178,7 @@ const ProfilePortfolioHeader = () => (
 const ProfilePortfolioItem = ({ title, link, company, date }) => (
   <div className="Profile__portfolio-item d-flex flex-column">
     <div className="Profile__portfolio-item-content flex-grow-1">
-      <h4 className="Profile__portfolio-title d-flex align-items-center">
+      <h4 className="Profile__label d-flex align-items-center">
         {title}
         {link && (
           <a href={link}>
@@ -231,11 +233,127 @@ const ProfileActivityTabs = () => (
     </div>
   </div>
 );
+const ProfileActivityItemInfo = ({ findings }) => (
+  <div className="Profile__activity-info d-flex align-items-center">
+    <div className="Profile__activity-total">
+      {findings.total} findings total
+    </div>
+    <div className="Profile__activity-findings d-flex align-items-center">
+      {findings.items.map((item) => {
+        const { icon, name, count } = item;
+        return (
+          <span
+            className="Profile__activity-type d-flex align-items-center"
+            key={JSON.stringify(item)}
+          >
+            <IconSvg name={icon} /> {count} {name}
+          </span>
+        );
+      })}
+    </div>
+  </div>
+);
+const ProfileActivityItem = ({ name, findings, avatar }) => (
+  <li className="Profile__activity-item">
+    <h3 className="Profile__label d-flex align-items-center">
+      <img
+        src={avatar}
+        className="Profile__activity-avatar position-relative"
+      />{" "}
+      {name} <IconSvg name="angle-arrow" />
+    </h3>
+    <ProfileActivityItemInfo findings={findings} />
+  </li>
+);
+// MOCK
+const activityItem1 = {
+  total: 16,
+  items: [
+    {
+      icon: "drop-critical",
+      name: "Critical findings",
+      count: 4,
+    },
+    {
+      icon: "drop-high",
+      name: "High finding",
+      count: 1,
+    },
+    {
+      icon: "drop-medium",
+      name: "Medium finding",
+      count: 1,
+    },
+    {
+      icon: "drop-low",
+      name: "Low findings",
+      count: 3,
+    },
+    {
+      icon: "drop-info",
+      name: "Information findings",
+      count: 7,
+    },
+  ],
+};
+const activityItem2 = {
+  total: 6,
+  items: [
+    {
+      icon: "drop-critical",
+      name: "Critical findings",
+      count: 1,
+    },
+    {
+      icon: "drop-low",
+      name: "Low findings",
+      count: 1,
+    },
+    {
+      icon: "drop-info",
+      name: "Information findings",
+      count: 2,
+    },
+  ],
+};
+const ProfileActivityList2 = () => (
+  <ul className="Profile__activity-list position-relative">
+    <ProfileActivityItem
+      name={"Security Audit for Polygon zkEVM"}
+      findings={activityItem1}
+      avatar={avatar2}
+    />
+  </ul>
+);
+const ProfileActivityList = () => (
+  <ul className="Profile__activity-list position-relative">
+    <ProfileActivityItem
+      name={"Security Audit for Polygon zkEVM"}
+      findings={activityItem1}
+      avatar={avatar2}
+    />
+    <ProfileActivityItem
+      name={"Security Audit for 1inch"}
+      findings={activityItem2}
+      avatar={avatar1}
+    />
+  </ul>
+);
 const ProfileActivity = () => (
   <div className="Profile__activity">
     <div className="Profile__activity-header">
       <ProfileSubtitle>Activity as Security Expert</ProfileSubtitle>
-      <ProfileActivityTabs />
+    </div>
+    <ProfileActivityTabs />
+    <div className="Profile__activity-content">
+      <div className="Profile__activity-month">
+        <ProfileSubtitle>{"March 2023"}</ProfileSubtitle>
+      </div>
+      <ProfileActivityList />
+      <div className="Profile__activity-month">
+        <ProfileSubtitle>{"March 2023"}</ProfileSubtitle>
+      </div>
+      <ProfileActivityList2 />
     </div>
   </div>
 );
